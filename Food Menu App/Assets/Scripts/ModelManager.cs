17b979867fd16afa592cbb7bbbc80336ba4d7bf6
Ovy Kode <ovidiu_metallica@yahoo.com>
2019-01-19 14:55:00 +0200

    Change Functionality

--- Food Menu App/Assets/Scripts/ModelManager.cs
+++ Food Menu App/Assets/Scripts/ModelManager.cs
@@ -19,11 +19,17 @@ public class ModelManager : MonoBehaviour {
     // _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
     private void Start()
     {
-        DisableAll();
+        DisableAll(); // Dezactivam modelele la pornirea aplicatiei.
     }
 
+    // _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+    // Method: DisableAll
+    // Desc: Folosita pentru dezactivarea tuturor modelelor din
+    //       array-ul models.
+    // _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
     private void DisableAll()
     {
+        // Fiecare item selectat din array-ul models se va dezactiva.
         foreach(var item in models)
         {
             item.SetActive(false);
