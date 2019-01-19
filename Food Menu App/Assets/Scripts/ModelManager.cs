7f6fdea0ed41daecc79c2aa07cb235e6c1faaab1
Ovy Kode <ovidiu_metallica@yahoo.com>
2019-01-19 14:50:54 +0200

    Update ModelManager.cs

--- Food Menu App/Assets/Scripts/ModelManager.cs
+++ Food Menu App/Assets/Scripts/ModelManager.cs
@@ -2,10 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+// Class: ModelManager
+// Desc: Contine toate metodele pentru functionalitatea
+//       sistemului de activare si dezactivare a modelelor
+//       3D atunci cand acestea sunt selectate din meniu.
+// _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
 public class ModelManager : MonoBehaviour {
 
     public GameObject[] models;
 
+    // _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+    // Method: Start
+    // Desc: Folosita pentru initializarea referintelor atunci
+    //       cand aplicatia porneste.
+    // _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
     private void Start()
     {
         DisableAll();
