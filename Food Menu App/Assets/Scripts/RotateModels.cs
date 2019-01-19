70d65cb6ded80720a2f40325923d6a7b210ce717
Ovy Kode <ovidiu_metallica@yahoo.com>
2019-01-19 15:07:11 +0200

    Update RotateModels.cs

--- Food Menu App/Assets/Scripts/RotateModels.cs
+++ Food Menu App/Assets/Scripts/RotateModels.cs
@@ -13,6 +13,11 @@ public class RotateModels : MonoBehaviour {
 
     float rotateSpeed = 100;
 
+    // _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+    // Method: OnMouseDrag
+    // Desc: Este apelata atunci cand utilizatorul da click 
+    //       (atinge)pe un elemenu GUI sau Collider
+    // _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
     private void OnMouseDrag()
     {
         float X = Input.GetAxis("Mouse X") * rotateSpeed * Mathf.Deg2Rad;
